b5c6cc39801f4fdd3bd63d3a8bf757ac6e991724
hottj <jhj13062004@naver.com>
2021-02-28 17:59:55 +0900

    키본 키 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,93 +6,17 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity // JPA가 처음 로딩될때 JPA가 사용되겠구나라고 인식
-@Table//(name="MBR") // DB의 MBR 테이블과 매핑
 
 public class Member {
 
     @Id // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name",nullable = false) // DB의 컬럼명은 name
     private String username; // 객체는 username
 
-    private Integer age;
-
-    @Enumerated(EnumType.STRING) // DB에는 Enum 타입이 없다. enum과 매핑
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate; // 년월
-    private LocalDateTime testLocalDateTime; // 년월일
-
-    @Lob // 큰 컨텐츠
-    private String description;
-
-    @Transient // DB에 적용안하고 메모리에만
-    private int temp;
-
     public Member() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
     }
 
     public Long getId() {
@@ -101,5 +25,13 @@ public class Member {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
